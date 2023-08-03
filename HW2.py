479f9c4f7949c6812add9c963e906dd74c517771
KvaskovSS <sava0980@yandex.ru>
2023-08-03 15:24:03 +0300

    Homework for seminar 3

---
+++ HW2.py
@@ -0,0 +1,105 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.next
+        print("None")
+
+    def reverse(self):
+        prev = None
+        current_node = self.head
+        while current_node:
+            next_node = current_node.next
+            current_node.next = prev
+            prev = current_node
+            current_node = next_node
+        self.head = prev
+
+# Пример использования:
+if __name__ == "__main__":
+    linked_list = LinkedList()
+    linked_list.append(1)
+    linked_list.append(2)
+    linked_list.append(3)
+    linked_list.append(4)
+
+    print("Исходный список:")
+    linked_list.print_list()
+
+    linked_list.reverse()
+    print("Развернутый список:")
+    linked_list.print_list()
+
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.prev = None
+        self.next = None
+
+class DoublyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+        new_node.prev = last_node
+
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" <-> ")
+            current_node = current_node.next
+        print("None")
+
+    def reverse(self):
+        current_node = self.head
+        while current_node:
+            temp = current_node.prev
+            current_node.prev = current_node.next
+            current_node.next = temp
+            current_node = current_node.prev
+        if temp:
+            self.head = temp.prev
+
+# Пример использования:
+if __name__ == "__main__":
+    doubly_linked_list = DoublyLinkedList()
+    doubly_linked_list.append(1)
+    doubly_linked_list.append(2)
+    doubly_linked_list.append(3)
+    doubly_linked_list.append(4)
+
+    print("Исходный список:")
+    doubly_linked_list.print_list()
+
+    doubly_linked_list.reverse()
+    print("Развернутый список:")
+    doubly_linked_list.print_list()
